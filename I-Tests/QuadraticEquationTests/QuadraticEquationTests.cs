e5bd0cb9d5a1e9ec4b29a3e27338f463ad9b6913
Егор Плеханов <egor.plekhanov@mail.ru>
2023-07-08 14:43:31 +0500

    Выполнен п.5. Добавлен тестовый метод для проверки метода Solve при уравнении x^2-1 = 0

--- I-Tests/QuadraticEquationTests/QuadraticEquationTests.cs
+++ I-Tests/QuadraticEquationTests/QuadraticEquationTests.cs
@@ -10,6 +10,9 @@ namespace QuadraticEquationTests
     [TestClass]
     public class QuadraticEquationTests
     {
+        private static string GetErrorMessage(string startMessage, double[] values) 
+            => $"{startMessage}. Метод вернул: ({string.Join(", ", values)})";
+
         /// <summary>
         /// Тест на то, что при x^2+1=0 метод вернёт пустой массив
         /// </summary>
@@ -23,7 +26,23 @@ namespace QuadraticEquationTests
 
             double[] actual = QuadraticEquation.Solve(a, b, c);
 
-            Assert.AreEqual(expected, actual, "Ошибка при вычислении выражения x^2+1=0");
+            Assert.AreEqual(expected, actual, GetErrorMessage("Ошибка при вычислении уравнения x^2+1=0", actual));
+        }
+
+        /// <summary>
+        /// Тест, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)
+        /// </summary>
+        [TestMethod]
+        public void AIs1_BIsZero_CIsMinus1()
+        {
+            double a = 1.0;
+            double b = 0.0;
+            double c = -1.0;
+            double[] expected = new[] { 1.0, -1.0 };
+
+            double[] actual = QuadraticEquation.Solve(a, b, c);
+
+            Assert.AreEqual(expected, actual, GetErrorMessage($"Ошибка при вычислении уравнения x^2-1 = 0", actual));
         }
     }
 }
