ed9fbf23cf614d09a4bc2d41c9ea317f0e60702b
Егор Плеханов <egor.plekhanov@mail.ru>
2023-07-08 15:13:15 +0500

    Выполнен п.7. Добавлен тест для проверки решения уравнения x^2+2x+1 = 0

--- I-Tests/QuadraticEquationTests/QuadraticEquationTests.cs
+++ I-Tests/QuadraticEquationTests/QuadraticEquationTests.cs
@@ -10,8 +10,11 @@ namespace QuadraticEquationTests
     [TestClass]
     public class QuadraticEquationTests
     {
-        private static string GetErrorMessage(string startMessage, double[] values) 
+        private static string GetErrorMessage(string startMessage, double[] values)
             => $"{startMessage}. Метод вернул: ({string.Join(", ", values)})";
+
+        private static string GetErrorMessage(string startMessage, double expectedValue, double actualValue)
+            => $"{startMessage}. x1 = {actualValue}, а ожидалось {expectedValue}";
 
         /// <summary>
         /// Тест на то, что при x^2+1=0 метод вернёт пустой массив
@@ -42,7 +45,29 @@ namespace QuadraticEquationTests
 
             double[] actual = QuadraticEquation.Solve(a, b, c);
 
-            Assert.AreEqual(expected, actual, GetErrorMessage($"Ошибка при вычислении уравнения x^2-1 = 0", actual));
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i], GetErrorMessage($"Ошибка при вычислении уравнения x^2-1 = 0", expected[i], actual[i]));
+            }
+        }
+
+        /// <summary>
+        /// Тест, который проверяет, что для уравнения x^2+2x+1 = 0 есть один корень кратности 2 (x1= x2 = -1)
+        /// </summary>
+        [TestMethod]
+        public void AIs1_BIs2_CIs1()
+        {
+            double a = 1.0;
+            double b = 2.0;
+            double c = 1.0;
+            double[] expected = new[] { -1.0, -1.0 };
+
+            double[] actual = QuadraticEquation.Solve(a, b, c);
+
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i], GetErrorMessage($"Ошибка при вычислении уравнения x^2+2x+1 = 0", expected[i], actual[i]));
+            }
         }
     }
 }
