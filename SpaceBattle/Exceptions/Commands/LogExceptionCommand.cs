41b68df01956636dab1a061e9737db39895a733a
Егор Плеханов <egor.plekhanov@mail.ru>
2023-09-02 14:08:43 +0500

    Реализован п.4 (реализовать команду, которая будет записывать ошибку в лог)

---
+++ SpaceBattle/Exceptions/Commands/LogExceptionCommand.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using SpaceBattle.Contracts;
+
+namespace SpaceBattle.Exceptions
+{
+    /// <summary>
+    /// Команда для записи исключения в лог
+    /// </summary>
+    public class LogExceptionCommand : ICommand
+    {
+        private readonly ILogger logger;
+        private readonly Exception exception;
+
+        public LogExceptionCommand(
+            ILogger logger,
+            Exception exception)
+        {
+            this.logger = logger;
+            this.exception = exception;
+        }
+
+        public void Execute()
+            => logger.Log(LogLevel.Error, exception.ToString());
+    }
+}
