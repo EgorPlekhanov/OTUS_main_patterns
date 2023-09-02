f348f05d88accc73e63b07fc821c447a2e704ebe
Егор Плеханов <egor.plekhanov@mail.ru>
2023-09-02 16:17:38 +0500

    Реализован п.6 (реализовать команду, которая повторяет команду, выбросившую исключение)

---
+++ SpaceBattle/Exceptions/Commands/RepeatFailedCommand.cs
@@ -0,0 +1,23 @@
+﻿using SpaceBattle.Contracts;
+
+namespace SpaceBattle.Exceptions.Commands
+{
+    /// <summary>
+    /// Команда, которая повторяет команду, которая завершилась с ошибкой
+    /// </summary>
+    public class RepeatFailedCommand : ICommand
+    {
+        private readonly ICommand failedCommand;
+
+        public RepeatFailedCommand(
+            ICommand failedCommand)
+        {
+            this.failedCommand = failedCommand;
+        }
+
+        public void Execute()
+        {
+            failedCommand.Execute();
+        }
+    }
+}
