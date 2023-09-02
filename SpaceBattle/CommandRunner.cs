daa79f677c2df012b7a3df837361683ee97d36f3
Егор Плеханов <egor.plekhanov@mail.ru>
2023-09-02 13:36:40 +0500

    Реализован раннер команд и обработчик ошибок

---
+++ SpaceBattle/CommandRunner.cs
@@ -0,0 +1,46 @@
+﻿using SpaceBattle.Contracts;
+using SpaceBattle.Exceptions;
+using System.Collections.Concurrent;
+
+namespace SpaceBattle
+{
+    /// <summary>
+    /// Раннер команд
+    /// </summary>
+    public class CommandRunner
+    {
+        /// <summary>
+        /// Очередь команд
+        /// </summary>
+        private readonly ConcurrentQueue<ICommand> queue;
+
+        /// <summary>
+        /// Обработчик ошибок
+        /// </summary>
+        private readonly ExceptionHandler exceptionHandler;
+
+        public CommandRunner(
+            ConcurrentQueue<ICommand> queue,
+            ExceptionHandler exceptionHandler)
+        {
+            this.queue = queue;
+            this.exceptionHandler = exceptionHandler;
+        }
+
+        public void Run()
+        {
+            while (!queue.IsEmpty)
+            {
+                _ = queue.TryDequeue(out ICommand command);
+                try
+                {
+                    command.Execute();
+                }
+                catch (Exception e)
+                {
+                    exceptionHandler.Handle(command, e).Execute();
+                }
+            }
+        }
+    }
+}
---
+++ SpaceBattle/Exceptions/ExceptionHandler.cs
@@ -0,0 +1,28 @@
+﻿using SpaceBattle.Contracts;
+
+namespace SpaceBattle.Exceptions
+{
+    /// <summary>
+    /// Обработчик ошибок
+    /// </summary>
+    public class ExceptionHandler
+    {
+        /// <summary>
+        /// Словарь (CommandType, Словарь(ExceptionType, HandleExceptionCommand))
+        /// </summary>
+        private readonly IDictionary<Type, IDictionary<Type, ICommand>> commandTypeExceptionHandlers;
+
+        public ExceptionHandler(
+            IDictionary<Type, IDictionary<Type, ICommand>> commandTypeExceptionHandlers)
+        {
+            this.commandTypeExceptionHandlers = commandTypeExceptionHandlers;
+        }
+
+        public ICommand Handle(ICommand command, Exception exception)
+        {
+            Type commandType = command.GetType();
+            Type exceptionType = exception.GetType();
+            return commandTypeExceptionHandlers[commandType][exceptionType];
+        }
+    }
+}
